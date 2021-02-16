24575af0437539a9602cca03b89c351d783788d1
vagrant <dsphere5296@gmail.com>
2021-02-16 16:13:34 +0000

    routes.rbの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,50 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	devise_for :admin, controllers: {
+		sessions: "admin/sessions",
+	}
+
+	namespace :admin do
+		get "top" => "home#top", as: "top"
+		get "search" => "home#search", as: "search"
+		get "customers/:customer_id/orders" => "orders#index", as: "customer_orders"
+		resources :customers, only: [:index, :show, :edit, :update]
+		resources :items, except: [:destory]
+		# "except"は７つの基本アクション中、不要なものを指定する。"only"は必要なものを指定する。
+		resources :genres, only: [:index, :create, :edit, :update]
+		resources :orders, only: [:index, :show, :update] do
+			resources :orders_details, only: [:update]
+		end
+		# "resources do"を用いて、ルーディングのネストをしている。
+		# ネストすると、URLの階層構造ができ、関係性のあるもの同士紐づけることができる。
+		# 参考文献：（https://techtechmedia.com/routing-nest-rails/）
+	end
+
+	devise_for :customers, controllers: {
+		sessions: "public/sessions",
+		registrations: "public/registrations",
+	}
+
+	scope module: :public do
+	# "scope module: :public"とすることで、URLはそのままにファイル構成のみ指定のパスに設定できる。
+	# 参考文献：（https://qiita.com/ryosuketter/items/9240d8c2561b5989f049）
+		root "items#top"
+		get "customers/mypage" => "customers#show", as: "mypage"
+		get "customers/information/edit" => "customers#edit", as: "edit_information"
+		patch "customers/information" => "customers#update", as: "update_information"
+		put "customers/information" => "customers#update"
+		get "customers/unsubscribe" => "customers#unsubscribe", as: "confirm_unsubscribe"
+		patch "customers/withdraw" => "customers#withdraw", as: "withdraw_customer"
+		put "customers/withdraw" => "customers#withdraw"
+		delete "cart_items/destroy_all" => "cart_items#destroy_all", as: "destroy_all_cart_items"
+		post "orders/confirm" => "orders#confirm"
+		get "orders/confirm" => "orders#error"
+		get "orders/thanks" => "orders#thanks", as: "thanks"
+
+		resources :address, only: [:index, :create, :edit, :update, :destroy]
+		resources :items, only: [:index, :show] do
+			resources :cart_items, only: [:create, :update, :destroy]
+		end
+		resources :cart_items, only: [:index]
+		resources :orders, only: [:new, :index, :create, :show]
+	end
 end
--- db/migrate/20210216153121_devise_create_customers.rb
+++ db/migrate/20210216153121_devise_create_customers.rb
@@ -33,7 +33,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.strign :first_name_kana, null: false
+      t.string :first_name_kana, null: false
       t.string :last_name_kana, null: false
       t.string :postal_code, null: false
       t.string :address, null: false
