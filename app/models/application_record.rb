78da4ded86145a349d64df0f4b670051eeabb37b
vagrant <dsphere5296@gmail.com>
2021-02-16 16:32:30 +0000

    application_record.rbにin_time_zoneメソッドの設定

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def created_at_jst
+  	created_at.in_time_zone("Tokyo")
+  end
+  # "created_at"の時間を東京時間に変換するメソッド。
 end
