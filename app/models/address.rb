a55568ebf08839de178bd5e57aafff1793b7ef69
vagrant <dsphere5296@gmail.com>
2021-02-16 16:26:41 +0000

    models/address.rbの記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,8 @@ class Address < ApplicationRecord
 	belongs_to :customer
 
 	validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+	# "format: {with: 正規表現}"とすることで、正規表現と属性の値が合致するかの検証ができる。今回は、郵便番号（ハイフンあり７桁）で指定。
+
 	validates :destination, presence: true
 	validates :name, presence: true
 
